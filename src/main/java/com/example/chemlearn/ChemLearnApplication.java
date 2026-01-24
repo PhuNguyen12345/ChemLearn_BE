262c2d0454fccf2dbb435383a2e185c98ba064cc
PhuNguyen12345 <phukak12345@gmail.com>
2026-01-24 17:36:30 +0700

    inital commit

---
+++ src/main/java/com/example/chemlearn/ChemLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.chemlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChemLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chemlearn/ChemLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chemlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChemLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
