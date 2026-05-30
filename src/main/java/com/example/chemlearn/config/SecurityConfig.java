106759863bfae95d456aa47e641d6c9a8cb6c62d
PhuNguyen12345 <phukak12345@gmail.com>
2026-05-30 11:52:19 +0700

    feat: update CORS origins.

--- src/main/java/com/example/chemlearn/config/SecurityConfig.java
+++ src/main/java/com/example/chemlearn/config/SecurityConfig.java
@@ -36,7 +36,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://103.153.254.119"));
+        configuration.setAllowedOrigins(
+                List.of("http://localhost:5173",
+                "http://103.153.254.119",
+                "https://chemlearn.com.vn",
+                "https://www.chemlearn.com.vn"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
